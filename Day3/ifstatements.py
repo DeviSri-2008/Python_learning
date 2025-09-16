4eed5e55066b6ef86cf60acd4468468ebcb2e77b
DeviSri-2008 <devisri190508@gmail.com>
2025-09-16 17:54:47 +0530

    Delete Day3 directory

--- Day3/ifstatements.py
+++
@@ -1,6 +0,0 @@
-is_hot =True  
-
-if is_hot:
-    print("it's a hot day")
-    print("drink plenty of water")
-    print("enjoy your day")
--- Day3/logicaloperators.py
+++
@@ -1,7 +0,0 @@
-has_high_income = True
-has_good_credit = true 
-
-if has_high-income and has_good_credit:
-print("eligible for loan")
-
-if has_high_income or has_good_credit
