2d4cfc8bb36e40d125ea29dd58db3724e2fc0b7f
DeviSri-2008 <devisri190508@gmail.com>
2025-09-16 17:38:29 +0530

    Add files via upload

---
+++ Day3/comoperator.ex.2.py
@@ -0,0 +1,7 @@
+name = "J"
+if len(name) <3:
+     print("name must be at least 3 character")
+elif len(name) > 50:
+     print("name must be a maximum of 50 caharacters")
+ else:
+      print(" name looks good")
